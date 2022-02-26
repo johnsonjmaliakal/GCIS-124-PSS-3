be03e50c1b56dfe88aea15a4d3382dbf4ff88fb7
johnsonjmaliakal <johnsonjmaliakal@gmail.com>
2022-02-26 14:20:55 +0400

    Updated visitDFS, still incomplete

--- AdjacencyRedBlackGraph.java
+++ AdjacencyRedBlackGraph.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -62,15 +63,49 @@ public class AdjacencyRedBlackGraph<E> implements RedBlackGraph<E> {
 
     @Override
     public boolean hasRedBlackPath(E a, E b) {
-        Set<RedBlackVertex<E>>visited=new HashSet<RedBlackVertex<E>>();
+        ArrayList<RedBlackVertex<E>>visited = new ArrayList<RedBlackVertex<E>>();
         RedBlackVertex<E>v=vertices.get(a);
         visitDFS(v,visited);
-        return visited.contains(vertices.get(b));
+        System.out.println(visited);                //extra
+        if (visited.contains(vertices.get(b))){
+            RedBlackVertex<E> first = vertices.get(a);
+            boolean temp = first.color();
+            int flag = 0; 
+            int starter = 0;
+            for (RedBlackVertex<E>i:visited){
+                if (starter == 0) {
+                    starter = 1;
+                    continue;
+                }
+                System.out.println(i);                  //extra
+                if (i.color() == !temp){
+                    temp = i.color();
+                    flag = 0;
+                }
+                else {
+                    flag = 1;
+                    break;
+                }
+                if (i == vertices.get(b)){
+                    break;
+                }
+            }
+            if (flag == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
     }
-    private void visitDFS(RedBlackVertex<E>v,Set<RedBlackVertex<E>>visited){
+    private void visitDFS(RedBlackVertex<E>v,ArrayList<RedBlackVertex<E>>visited){
         visited.add(v);
         for(RedBlackVertex<E>w:v.getNeighbors()){
-            if(!visited.contains(w))
+            RedBlackVertex<E> test = visited.get(visited.size()-1);
+            if(!visited.contains(w) && test.color() != w.color())
                 visitDFS(w,visited);
         }
     }
